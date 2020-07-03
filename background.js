1860bf2da7f39b1989b2c59774e1438276289099
benjhardy <benjamin.m.hardy@vanderbilt.edu>
2020-07-02 20:54:20 -0500

    Just learning how chrome API's and JS works, not much progress

--- background.js
+++ background.js
@@ -1,36 +1,33 @@
 // coding train
-console.log("background is running")
+console.log("background is running");
 // console.log(tab.id)
 
-/*
-//chrome.tabs.getCurrent(redirect)
-// chrome.tabs.sendMessage(tab.url);
-function redirect(tab) {
-  if (tab.url === "https://www.instagram.com/") {
-        
-        console.log("Gonna Cancel this page real quick")
-        window.open("https://vuiis.vumc.org/~hardybm/")
-        console.log(tab.id)
-        chrome.tabs.remove(tab.id)
-    }
-}
-*/
-chrome.browserAction.onClicked.addListener(buttonClicked)
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    id: "sampleContextMenu",
+    title: "Sample Context Menu",
+    contexts: ["selection"],
+  });
+});
 
+chrome.browserAction.onClicked.addListener(buttonClicked);
 function buttonClicked(tab) {
-  console.log(tab.url)
+  console.log(tab.url);
   var msg = {
     message: tab.url,
-    id: tab.id
-  }
+    id: tab.id,
+  };
   chrome.tabs.sendMessage(tab.id, msg);
 
   if (msg.message === "https://www.instagram.com/") {
-        
-        console.log("Gonna Cancel this page real quick")
-        window.open("https://vuiis.vumc.org/~hardybm/")
-        console.log(msg.id)
-        chrome.tabs.remove(msg.id)
-    }
+    console.log("Gonna Cancel this page real quick");
+    window.open("https://vuiis.vumc.org/~hardybm/");
+    console.log(msg.id);
+    chrome.tabs.remove(msg.id);
+  }
 }
 
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  let url = tabs[0].url;
+  // use `url` here inside the callback because it's asynchronous!
+});
--- content.js
+++ content.js
@@ -1,18 +1,16 @@
-console.log("Hello world")
-
+console.log("Hello world");
+/*
 chrome.tabs.getCurrent(function(tab){
-    console.log(tab.url);
-  });
-
+//    console.log(tab.url);
+//  });
+*/
 chrome.runtime.onMessage.addListener(receiver);
 
 function receiver(request, sender, sendResponse) {
-    
-    if (request.message === "https://www.instagram.com/") {
-        
-        console.log("Gonna Cancel this page real quick")
-        //window.open("https://vuiis.vumc.org/~hardybm/")
-        //console.log(request.id)
-        //chrome.tabs.remove(request.id)
-    }
+  if (request.message === "https://www.instagram.com/") {
+    console.log("Gonna Cancel this page real quick");
+    //window.open("https://vuiis.vumc.org/~hardybm/")
+    //console.log(request.id)
+    //chrome.tabs.remove(request.id)
+  }
 }
